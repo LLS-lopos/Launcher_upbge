305a38d64868b837e26d24cff8934f899ed73a95
LLS-lopos <luderickls@gmail.com>
2025-03-19 22:20:27 -0400

    réajustement du listage des projets

--- source/GUI/liste_projet.py
+++ source/GUI/liste_projet.py
@@ -1,23 +1,34 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QTabWidget, QListWidget
 from PySide6.QtGui import QIcon
 
+from pathlib import Path
+
+from source.program.manipuler_donner import charger
 
 class Lprojet(QWidget):
     def __init__(self):
         super().__init__()
-
+        icone = charger("icon")
+        print(icone)
+        layout = QVBoxLayout()
         tableau = QTabWidget(self)
         tableau.setMaximumWidth(300)
 
-        #page
-        p1 = QListWidget()
-        p1.addItem("test")
+        linux = charger("linux")
+        for v_projet in linux["projet"]:
+            page = QListWidget() # page (élément de tableau)
+            for projet in linux["projet"][v_projet]:
+                if projet:
+                    page.addItem(Path(projet).name)
+                tableau.addTab(page, QIcon(icone.get("linux")), str(v_projet)) # ajouter page au tableau
+        
+        windows = charger("windows")
+        for v_projet in windows["projet"]:
+            page = QListWidget() # page (élément de tableau)
+            for projet in windows["projet"][v_projet]:
+                if projet:
+                    page.addItem(Path(projet).name)
+                tableau.addTab(page, QIcon(icone.get("windows")), str(v_projet)) # ajouter page au tableau
 
-        #tableau
-        tableau.addTab(p1, QIcon(""), "P1")
-
-        layout = QVBoxLayout()
         layout.addWidget(tableau)
         self.setLayout(layout)
-
-
